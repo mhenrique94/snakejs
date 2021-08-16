0eba14900a196014331a4e6dec1f314ba5e4345d
mhenrique94 <mhenrique94@outlook.com>
2021-08-16 00:39:10 -0300

    definição da direção da cobrinha

--- script.js
+++ script.js
@@ -23,7 +23,21 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+
+    if(cobrinha[0].x > 15 * box && direction == "right") cobrinha[0].x = 0;
+    if(cobrinha[0].x < 0 && direction == "left") cobrinha[0].x = 16*box;
+    if(cobrinha[0].y > 15 * box && direction == "down") cobrinha[0].y = 0;
+    if(cobrinha[0].y < 0 && direction =="up") cobrinha[0].y = 16 * box;
     criarBG();
     criarCobrinha();
 
